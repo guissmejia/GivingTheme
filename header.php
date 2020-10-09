5462b734104800af270cd01aca2b9dde8dbb9a80
Guiselle Mejia <guissmejia@gmail.com>
2020-10-09 16:08:49 -0500

    The latest news section is created on the home page

--- header.php
+++ header.php
@@ -6,15 +6,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <?php wp_head() ?>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css"> 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
 </head>
 
 <body <?php body_class(); ?>>
-  <header class="Header">
-    <div class="Header__title">
-      <h1>Adam</h1>
+  <header>
+    <section class="Header">
+      <div class="Header__title">
+        <h1>Adam</h1>
+      </div>
+      <div class="Header__icon--menu">
+        <i class="fas fa-bars"></i>
+    </section>
     </div>
-    <div class="Header__icon--menu">
-      <i class="fas fa-bars"></i>
+    <figure class="Header__main--image">
+      <img src="<?php echo get_template_directory_uri()?>/assets/main-image.jpg" alt="Main image Home" />
+    </figure>
+    <div>
+      <?php
+      wp_nav_menu(
+        array(
+          'theme_location' => 'header_menu',
+          'container_class' => 'header-nav-menu',
+          'container' => 'nav'
+        ));
+        ?>
     </div>
- </header>
+  </header>
--- page-templates/giving-home.php
+++ page-templates/giving-home.php
@@ -6,4 +6,45 @@ Template Name: Giving Home
 
 <?php get_header(); ?>
 
+<section class="Home">
+  <section class="Home__latest__publications">
+		<article class="Latests__publications--title">
+			<h2>Latests Posts</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+		</article>
+    <ul>
+      <?php
+					query_posts('category_name=latests-posts&posts_per_page=3');
+					if (have_posts()) : while (have_posts()) : the_post();
+					?>
+      <li>
+        <a href="<?= get_permalink(); ?>" title="<?php the_title(); ?>">
+          <?php the_post_thumbnail('medium'); ?>
+          <article class="Article__description">
+            <h3><?php the_title(); ?></h3>
+            <span><?php the_time('j F, Y') ?></span>
+            <?php the_excerpt(); ?>
+          </article>
+          <div class="Article--actions">
+            <div>
+              <span class="icons"><i class="fas fa-sync-alt"></i></span>
+              <span class="icons"><i class="far fa-comments"></i></span>
+              <span class="icons"><i class="far fa-heart"></i></span>
+            </div>
+            <p>Read more</p>
+          </div>
+        </a>
+      </li>
+      <?php
+					endwhile;
+				else :
+					?>
+      <article>
+        <p>No hay contenido a mostrar </p>
+      </article>
+      <?php endif; ?>
+    </ul>
+  </section>
+</section>
+
 <?php get_footer(); ?>
